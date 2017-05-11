568dff4b83ac49ecd1b5023457358b69647813fe
Dominic Brown and Nicole Santamaria <nicole@>
2017-05-11 15:26:45 -0700

    add many tests that all pass perfectly

--- spec/qvipp_spec.rb
+++ spec/qvipp_spec.rb
@@ -1,10 +1,26 @@
 require "spec_helper"
 
-# describe 'Palindrome#isWord?' do
-#   let(:word) { Palindrome.new }
-#
-#   it "returns true if the word has at least one vowel" do
-#     expect(word.is_word?("word")).to eq true
-#   end
-#
-# end
+
+describe Qvipp do
+
+### RELATIONSHIPS
+  it { should have_many(:users) }
+  it { should have_many(:feeds) }
+  it { should have_many(:users).through(:feeds) }
+
+  it ("has many users") do
+    qvipp1 = Qvipp.create({:haiku => "i do dis"})
+    user1 = User.create({:name => 'jim', :email => 'jim@email.com', :qvipp_ids => [qvipp1.id]})
+    user2 = User.create({:name => 'jillian', :email => 'jillian@email.com', :qvipp_ids => [qvipp1.id]})
+    expect(qvipp1.users()).to(eq([user1, user2]))
+  end
+################
+
+  it ("the callback downcases the haiku after saving") do
+    qvipp1 = Qvipp.create({:haiku => "I's da BEST"})
+    expect(qvipp1.haiku).to(eq("i's da best"))
+  end
+
+  
+
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 describe User do
 
+### RELATIONSHIPS
   it { should have_many(:qvipps) }
   it { should have_many(:feeds) }
   it { should have_many(:qvipps).through(:feeds) }
@@ -12,10 +13,26 @@ describe User do
     qvipp2 = Qvipp.create({:haiku => "i am dis", :user_ids => [user1.id]})
     expect(user1.qvipps()).to(eq([qvipp1, qvipp2]))
   end
+################
+
+#### VALIDATION
+  it ("checks for true email using @ char") do
+    user1 = User.new({:name => 'jim', :email => 'jimmmyboy'})
+    user1.save()
+    expect(user1.errors.any?).to(eq(true))
+  end
+
+  it ("checks for true email using @ char") do
+    user1 = User.new({:name => 'jim', :email => 'jimmm@syboy'})
+    user1.save()
+    expect(user1.errors.any?).to(eq(false))
+  end
+
+  it ("checks for true email using @ char") do
+    user1 = User.new({:name => 'jim', :email => 'jimmyboy'})
+    user1.save()
+    expect(user1.errors.to_hash[:email][0]).to(eq("That's no email foo!"))
+  end
+#################
 
 end
-
-# RSpec.describe User, type: :model do
-#   it { should have_many(:qvipps) }
-#   it { should have_many(:feeds) }
-# end
